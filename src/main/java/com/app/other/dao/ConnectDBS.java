89a16a1a149020d51cc7c49270469294b4dda578
Sergiu Pacurariu <sergiu_pacurariu@yahoo.com>
2017-06-04 18:53:54 +0300

    updated database connection

--- src/main/java/com/app/other/dao/ConnectDBS.java
+++ src/main/java/com/app/other/dao/ConnectDBS.java
@@ -9,10 +9,10 @@ import java.sql.SQLException;
 public class ConnectDBS {
 
 	//connection to local dbs
-//	private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/sugestiaDB?user=postgres&password=admin";
+	//private static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/sugestiaDB?user=postgres&password=admin";
 	
 	//connection to Heroku dbs
-	private static final String DATABASE_URL = "jdbc:postgresql://ec2-54-75-238-7.eu-west-1.compute.amazonaws.com:5432/d7uspq8tpsf5g?sslmode=require&user=ruighabjkqpgan&password=P84k93A2Vvvu5pnXc31pHHoVPN";
+	private static final String DATABASE_URL = "jdbc:postgresql://ec2-23-21-235-142.compute-1.amazonaws.com:5432/d7m1ps4lh63rr2?sslmode=require&user=nmiszbnpsaklwg&password=29404ab99aac6cb4b9401307a44f16886f67f7aedc84a12dd8c3af1f53afb6d0";
 	
 	static Connection connection = null;
 	PreparedStatement preparedStatement = null;
